d6fcb7e9f8ff1626f01ab03f0398c2045cca77ed
Rachid SAOUDI <rachid.saoudi@atos.net>
2023-04-05 16:35:46 +0000

    initial commit

---
+++ src/main/java/ma/saoudi/tuto_springboot_jdbc/TutoSpringbootJdbcApplication.java
@@ -0,0 +1,13 @@
+package ma.saoudi.tuto_springboot_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoSpringbootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoSpringbootJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/saoudi/tuto_springboot_jdbc/TutoSpringbootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.saoudi.tuto_springboot_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoSpringbootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
